ffec88699871c90126af688294968f65f54d3a27
Johanna Corre <johanna.corre@isen.yncrea.fr>
2023-03-10 03:31:34 +0100

    Ajout requete GET pour récupérer les données

--- src/components/InfosChoice/InfosChoice.js
+++ src/components/InfosChoice/InfosChoice.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./InfosChoice.css";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import tokenResponse from '../../data/data.json';
 
 export default function InfosChoice() {
+
+    const [test, setTest] = useState()
+    
+    const token = tokenResponse["access_token"];
+    axios.get('https://api-noe.alerteo.com/noe/v1.1/sites/10076/compteurs', {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then(response => {
+        console.log('Response:', response);
+        // filesystem.writeFile('response.json', response.data, function (err) {
+        //     console.log(err);
+        // });
+        setTest(response.data[0]["id"])
+    })
+    .catch(error => console.error(error));
+
+
     return (
         <div className='infos-choice-container'>
             <Link to="/" style={{ color: "red"}}>
@@ -14,6 +34,7 @@ export default function InfosChoice() {
             <Link to="/" style={{ color: "blue"}}>
             <i class="fa-sharp fa-solid fa-droplet"></i>
             </Link>
+            <h1>{test}</h1>
         </div>
     )
 }
